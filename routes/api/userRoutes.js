3761e1f537d7d28645648f1a42409c46dab612d8
Kcrespo15 <kevincrespo15@gmail.com>
2022-10-12 17:06:43 -0400

    did routes

---
+++ routes/api/userRoutes.js
@@ -0,0 +1,26 @@
+const router = require('express').Router();
+const {
+  getUsers,
+  getSingleUser,
+  createUser,
+  updateUser,
+  removeUser,
+  addFriend,
+  removeFriend
+} = require('../../controllers/userController');
+
+// /api/users
+router.route('/')
+  .get(getUsers)
+  .post(createUser);
+
+// /api/users/:userId
+router.route('/:userId')
+  .get(getSingleUser)
+  .put(updateUser)
+  .delete(removeUser)
+
+//api/users/:userId/:friendId
+router.route('/:userId/:friendId')
+  .post(addFriend)
+  .delete(removeFriend);
